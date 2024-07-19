8f74f4f047387670d433fa1880af7e5482d8376e
Iñigo <ivarelamar1@gmail.com>
2024-07-19 10:14:09 +0200

    Prueba api

---
+++ PruebaApiClientes/Clientes/Clientes.cs
@@ -0,0 +1,7 @@
+﻿namespace Clientes
+{
+    public class Clientes
+    {
+
+    }
+}
---
+++ PruebaApiClientes/PruebaApiClientes/Controllers/ClientesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PruebaApiClientes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult getPrueba()
+        {
+           // Worker worker = new Worker();
+            return Ok();
+        }
+        
+       
+    }
+}
