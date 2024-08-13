2f559c260ab8559b3c2851ec75c14c1f51aed4da
Iñigo <ivarelamar1@gmail.com>
2024-08-13 13:00:01 +0200

    #83 Añadido tailwind

---
+++ ApiClases_20240722_Solucion/Frontal/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app/registro/*.{html,js}", "./src/app/login/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
