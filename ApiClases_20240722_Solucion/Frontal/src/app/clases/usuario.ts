da50e6a391d11673db6becac11d075fa8a6cbb0b
iñigo varela <ivarelamar1@gmail.com>
2024-08-13 12:21:29 +0000

    Revert "Revert "Cambios staged""

---
+++ ApiClases_20240722_Solucion/Frontal/src/app/clases/usuario.ts
@@ -0,0 +1,11 @@
+export class Usuario {
+    id!: string;
+    userName!: string;
+    email!: string;
+    rol!: string;
+    passwordHash!: string;
+    securityStamp!: string;
+    concurrencyStamp!: string;
+    paisId!:number
+    
+  }
