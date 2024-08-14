0ff047e35aa2684d096e022f67ce643be4608e3a
Obelisko <orodaxrome65@gmail.com>
2024-08-14 09:46:12 +0200

    #94 Creacion de Entidad, Modelo y añadido

---
+++ ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Modelos/PaisDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ApiClases_20270722_Proyecto.Modelos
+{
+    public class PaisDto
+    {
+        public int PaisId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [StringLength(50)]
+        public string Divisa { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        public string Iso3 { get; set; }
+    }
+}
---
+++ ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Profiles/PaisProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiClases_20270722_Proyecto.Profiles
+{
+    public class PaisProfile : Profile
+    {
+        public PaisProfile()
+        {
+            CreateMap<Pais, PaisDto>().ReverseMap();
+        }
+    }
+}
