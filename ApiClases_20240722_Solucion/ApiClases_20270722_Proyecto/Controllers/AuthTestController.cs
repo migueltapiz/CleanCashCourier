0f0bc596e9a83400211e44b2d73333266ee62b46
Avertcoiin <vertiz.138863@e.unavarra.es>
2024-08-19 13:12:25 +0200

    #132 Confirmación del correcto funcionamiento. Se ha añadido el controlador AuthTestController.cs que se usa para confirmar que funciona en el postman y más tarde habrá que quitarlo.

---
+++ ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Controllers/AuthTestController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ApiClases_20270722_Proyecto.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthTestController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public AuthTestController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet("generate-token")]
+    public IActionResult GenerateToken()
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "testuser")
+            }),
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            Issuer = _configuration["Jwt:Issuer"],
+            Audience = _configuration["Jwt:Audience"]
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var tokenString = tokenHandler.WriteToken(token);
+
+        return Ok(new { Token = tokenString });
+    }
+}
