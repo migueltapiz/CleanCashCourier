91faf6ec464b7a9f326ebdd99d5f331cf3799828
Iñigo <ivarelamar1@gmail.com>
2024-08-19 09:12:05 +0200

    #125 Cambios para evitar conflictos

--- ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Controllers/ClientesController.cs
+++ ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Controllers/ClientesController.cs
@@ -4,39 +4,34 @@ namespace ApiClases_20270722_Proyecto.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ClientesController : ControllerBase{
-    public readonly IRepositorioGenerico<Cliente> repositorio;
+public class ClientesController : ControllerBase
+{
+    public readonly IClienteRepositorio repositorio;
     private readonly IMapper _mapper;
-    public ClientesController(IRepositorioGenerico<Cliente> repositorio,IMapper mapper){
+    public ClientesController(IClienteRepositorio repositorio, IMapper mapper) {
         this.repositorio = repositorio;
         _mapper = mapper ??
                throw new ArgumentNullException(nameof(mapper));
     }
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<ClienteDto>>> Get(){
-
-
-        //var clientes = await repositorio.ObtenerClientes();
-        //var clientesDto = _mapper.Map<IEnumerable<ClienteDto>>(clientes);
-
-        return Ok(_mapper.Map<IEnumerable<ClienteDto>>(await repositorio.Obtener()));
-        //return Ok(clientesDto);
+    public async Task<ActionResult<IEnumerable<ClienteDto>>> Get() {
+        return Ok(_mapper.Map<IEnumerable<ClienteDto>>(await repositorio.ObtenerClientes()));
     }
 
 
     [HttpGet("{id}", Name = "getCliente")]
     public ActionResult<ClienteDto> Get(int id) {
-        var cliente = repositorio.ObtenerPorId(id);
+        var cliente = repositorio.ObtenerClienteId(id);
         var finalClienteDto = _mapper.Map<ClienteDto>(cliente);
         return finalClienteDto == null ? NotFound() : Ok(finalClienteDto);
     }
 
     [HttpPost]
     public async Task<ActionResult<ClienteDto>> PostAsync(ClienteDto cliente) {
-        var finalClienteNuevo =_mapper.Map<ClienteDto,Cliente>(cliente);
+        var finalClienteNuevo = _mapper.Map<ClienteDto, Cliente>(cliente);
         repositorio.Agregar(finalClienteNuevo);
-       
-        return await repositorio.GuardarCambios()? Ok("Cliente añadido correctamente"): BadRequest();
+
+        return await repositorio.GuardarCambios() ? Ok("Cliente añadido correctamente") : BadRequest();
 
     }
 
@@ -53,5 +48,3 @@ public class ClientesController : ControllerBase{
         return await repositorio.GuardarCambios() ? Ok("Cliente borrado correctamente") : BadRequest();
     }
 }
-
-
--- ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Repositorios/ClienteRepositorioBBDD.cs
+++ ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Repositorios/ClienteRepositorioBBDD.cs
@@ -1,34 +1,37 @@
-﻿using ApiClases_20270722_Proyecto.ContextoCarpeta;
+﻿namespace ApiClases_20270722_Proyecto.Repositorios;
 
-namespace ApiClases_20270722_Proyecto.Repositorios;
-
-public class ClienteRepositorioBBDD<T> : IRepositorioGenerico<T> where T:Cliente {
+public class ClienteRepositorioBBDD : IClienteRepositorio
+{
     private readonly Contexto _contexto;
 
     public ClienteRepositorioBBDD(Contexto contexto) {
         _contexto = contexto;
     }
-    public void Actualizar(int id, T cliente) {
+    public void Actualizar(int id, Cliente cliente) {
         cliente.Id = id;
         _contexto.Clientes.Update(cliente);
     }
-    public void Agregar(T cliente) {
+    public void Agregar(Cliente cliente) {
         _contexto.Clientes.Add(cliente);
     }
     public void Borrar(int id) {
         var cliente = _contexto.Clientes.FirstOrDefault(c => c.Id == id);
         _contexto.Clientes.Remove(cliente);
     }
+    public Cliente ObtenerClienteId(int id) {
+        return _contexto.Clientes.Include(c => c.Pais).FirstOrDefault(c => c.Id == id);
+    }
+    public async Task<List<Cliente>> ObtenerClientes() {
+        return await _contexto.Clientes.Include(c => c.Pais).ToListAsync();
 
+    }
 
     public async Task<bool> GuardarCambios() {
 
         return await _contexto.SaveChangesAsync() > 0;
+
     }
 
-    public async Task<List<T>> Obtener() => await _contexto.Set<T>().Include(c => c.Pais).ToListAsync();
 
-    public T ObtenerPorId(int id) =>_contexto.Set<T>().FirstOrDefault(c => c.Id == id);
-    public Task<List<T>> ObtenerTodosFiltrado(int id_cliente, DateTime? fechaInicio, DateTime? fechaFin, double? cantidadEnviadaMin, double? cantidadEnviadaMax, double? cantidadRecibidaMin, double? cantidadRecibidaMax) => throw new NotImplementedException();
-    public T ObtenerTransaccionId(int id_cliente, int id_transaccion) => throw new NotImplementedException();
+
 }
