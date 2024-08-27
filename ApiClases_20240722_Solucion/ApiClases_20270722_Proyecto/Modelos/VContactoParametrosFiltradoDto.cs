43158bc55a3ffa3871656aa94af88c3618baa812
Obelisko <orodaxrome65@gmail.com>
2024-08-27 12:23:18 +0200

    Added filtro con parametros para vista

---
+++ ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Modelos/VContactoParametrosFiltradoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiClases_20270722_Proyecto.Modelos
+{
+    public class VContactoParametrosFiltradoDto
+    {
+        public int ClienteId { get; set; }
+        public string? username { get; set; }
+        public int? PaisId { get; set; }
+        public int NumeroPaginas { get; set; } = 1;
+        public int TamanoPagina { get; set; } = 10;
+    }
+}
