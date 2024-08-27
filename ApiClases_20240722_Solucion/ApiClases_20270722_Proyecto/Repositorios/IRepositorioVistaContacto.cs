35c0a2a482c52e247d69293d3a92ef18a7fb0e7a
Obelisko <orodaxrome65@gmail.com>
2024-08-27 12:50:05 +0200

    Subido repositorio vista contactos

---
+++ ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Repositorios/IRepositorioVistaContacto.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiClases_20270722_Proyecto.Repositorios
+{
+    public interface IRepositorioVistaContacto<VContacto>
+    {
+        Task<(List<VContacto> Data, int TotalCount)> GetVContactosAsync(VContactoParametrosFiltradoDto parametrosfiltro);
+    }
+}
