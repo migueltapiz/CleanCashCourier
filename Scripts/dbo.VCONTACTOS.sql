496e8e3930a30b3db1feffd3d18782d3be0538da
Obelisko <orodaxrome65@gmail.com>
2024-08-27 14:29:28 +0200

    Añadido script de creacion de vista

---
+++ Scripts/dbo.VCONTACTOS.sql
@@ -0,0 +1,11 @@
+﻿
+                    CREATE VIEW VCONTACTOS
+                    AS SELECT
+                      Clientes.Id AS IdCliente
+                     ,Clientes_1.Usuario AS NombreContacto
+                     ,Clientes_1.PaisId
+                    FROM dbo.Contactos
+                    INNER JOIN dbo.Clientes
+                      ON Contactos.ClienteOrigenId = Clientes.Id
+                    INNER JOIN dbo.Clientes Clientes_1
+                      ON Contactos.ClienteDestinoId = Clientes_1.Id;
