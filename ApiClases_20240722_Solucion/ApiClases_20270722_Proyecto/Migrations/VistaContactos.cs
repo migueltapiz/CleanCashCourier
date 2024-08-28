1906c08c61994d1034153cfb031c1d1bf676ef12
Obelisko <orodaxrome65@gmail.com>
2024-08-28 10:19:21 +0200

    #180 cambiada estructura de la vista

--- ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Migrations/VistaContactos.cs
+++ ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Migrations/VistaContactos.cs
@@ -7,16 +7,18 @@ namespace ApiClases_20270722_Proyecto.Migrations
         public void CrearVistas(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-                    CREATE VIEW VCONTACTOS
+                    CREATE VIEW VCONTACTOS 
                     AS SELECT
-                      Clientes.Id AS IdCliente
-                     ,Clientes_1.Usuario AS NombreContacto
-                     ,Clientes_1.PaisId
+                      Clientes_Origen.Id AS IdCliente
+                     ,Clientes_Contacto.Usuario AS NombreContacto
+                     ,Paises_Contacto.Nombre AS Pais
                     FROM dbo.Contactos
-                    INNER JOIN dbo.Clientes
-                      ON Contactos.ClienteOrigenId = Clientes.Id
-                    INNER JOIN dbo.Clientes Clientes_1
-                      ON Contactos.ClienteDestinoId = Clientes_1.Id;
+                    INNER JOIN dbo.Clientes Clientes_Origen
+                      ON Contactos.ClienteOrigenId = Clientes_Origen.Id
+                    INNER JOIN dbo.Clientes Clientes_Contacto
+                      ON Contactos.ClienteDestinoId = Clientes_Contacto.Id
+                    INNER JOIN dbo.Paises Paises_Contacto
+                      ON Clientes_Contacto.PaisId = Paises_Contacto.Id;
                 ");
         }
         public void EliminarVistas(MigrationBuilder migrationBuilder)
