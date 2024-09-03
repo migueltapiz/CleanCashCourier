647bb356c9983cf41b2ad6f523daeb6d931d13ab
Obelisko <orodaxrome65@gmail.com>
2024-09-03 12:47:41 +0200

    Mejorando experiencia de usuario añadiendo respuestas a usuario inexistente o contraseña incorrecta

--- ApiClases_20240722_Solucion/Frontal/src/app/login/login.component.ts
+++ ApiClases_20240722_Solucion/Frontal/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Usuario } from '../interfaces/usuario';
 import { Router } from '@angular/router';
 import { ClienteService } from '../servicios/cliente.service';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-login',
@@ -18,37 +19,40 @@ export class LoginComponent {
   constructor(private clienteService: ClienteService, private route: Router) { }
 
   authentication() {
-    this.clienteService.autenticarUsuario(this.email, this.contrase, true).subscribe(response => {
-      if (response && response.token) {
+    this.clienteService.autenticarUsuario(this.email, this.contrase, true).subscribe(
+      response => {
+        if (response && response.token) {
         localStorage.setItem('token', response.token);
         this.route.navigate(['/welcome']); // Redirige a la ruta protegida
       } else {
         alert('Credenciales incorrectas');
         console.log(response);
       }
-    });
+      },
+      error => {
+        if (error.status === 404) {
+          //alert('No existe el usuario');
+          this.showUserDoesntExistModal();
+        } else if (error.status === 401) {
+          this.showIncorrectPasswordModal();
+          //alert('Contraseña incorrecta');
+        } else if (error.status === 0) {
+          alert('No se pudo conectar al servidor.');
+        } else {
+          console.error(`Error ${error.status}: ${error.message} --- ${error.error.message}`);
+          alert('Error en el proceso de inicio de sesion: ' + error.message);
+        }
+      });
   }
-  //this.clienteService.registrarCliente(clienteRegistro).subscribe(
-  //  next => {
-  //    this.router.navigate(['/login']);
-  //  },
-  //  error => {
-  //    console.log(error);
-  //    console.log(error.error.message)
-  //    console.error(error);
-  //    if (error.status === 400) {
-  //      console.error('Error de validación', error);
-  //      alert('Error al registrar usuario: ' + error.error);
-  //    } else if (error.status === 409) {
-  //      alert('El email ya existe en CleanCashCourier, intentelo de nuevo');
-  //    } else if (error.status === 0) {
-  //      alert('No se pudo conectar al servidor.');
-  //    } else {
-  //      console.error(`Error ${error.status}: ${error.message} --- ${error.error.message}`);
-  //      alert('Error en el proceso de registro: ' + error.message);
-  //    }
-  //  },
 
-  //);
-  
+  showUserDoesntExistModal() {
+    const modalElement = document.getElementById('userNoExists');
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+  }
+  showIncorrectPasswordModal() {
+    const modalElement = document.getElementById('incorrectPassword');
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+  }
 }
