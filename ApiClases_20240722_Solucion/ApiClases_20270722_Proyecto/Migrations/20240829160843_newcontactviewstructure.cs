f55076697bbbffb3186ccbf46260a7bfde1d199f
Miguel Tapiz <mtmcomplex@gmail.com>
2024-09-05 11:10:33 +0200

    #216 Procedimientos almacenados en migraciones

--- ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Migrations/20240829160843_newcontactviewstructure.cs
+++ ApiClases_20240722_Solucion/ApiClases_20270722_Proyecto/Migrations/20240829160843_newcontactviewstructure.cs
@@ -663,6 +663,7 @@ namespace ApiClases_20270722_Proyecto.Migrations
                 table: "Contactos",
                 column: "ClienteOrigenId");
             CrearVistas(migrationBuilder);
+            ProcedimientosAlmacenados(migrationBuilder);
         }
 
         /// <inheritdoc />
@@ -704,6 +705,7 @@ namespace ApiClases_20270722_Proyecto.Migrations
             migrationBuilder.DropTable(
                 name: "Paises");
             EliminarVistas(migrationBuilder);
+            EliminarProcedimientosAlmacenados(migrationBuilder);
         }
     }
 }
