ee55721344c0602b4f5f41a64e85e5a8edfe18f6
Miguel Tapiz <mtmcomplex@gmail.com>
2024-09-05 10:46:12 +0200

    #214 Implementación procedimientos almacenados en welcome

--- ApiClases_20240722_Solucion/Frontal/src/app/welcome-component/welcome-component.component.ts
+++ ApiClases_20240722_Solucion/Frontal/src/app/welcome-component/welcome-component.component.ts
@@ -3,17 +3,28 @@ import { Router } from '@angular/router';
 import { CabeceraComponent } from '../cabecera/cabecera.component'
 import jwt from 'jsonwebtoken';
 import { jwtDecode } from "jwt-decode";
+import { DataService } from '../servicios/data.service';
 @Component({
   selector: 'welcome-component',
   templateUrl: './welcome-component.component.html',
   styleUrl: './welcome-component.component.css'
 })
 export class WelcomeComponentComponent implements OnInit{
-
+  paisesConClientes: number | undefined;
+  transaccionesUltimos10Anios: number | undefined;
   token: any;
-  constructor(private router: Router) { }
+  constructor(private router: Router, private dataService: DataService) { }
     ngOnInit(): void {
-      
+      // Llamar a los servicios para obtener los datos
+      this.dataService.getPaisesConClientes().subscribe({
+        next: (data) => this.paisesConClientes = data,
+        error: (err) => console.error('Error obteniendo países:', err)
+      });
+
+      this.dataService.getTransaccionesUltimos10Anios().subscribe({
+        next: (data) => this.transaccionesUltimos10Anios = data,
+        error: (err) => console.error('Error obteniendo transacciones:', err)
+      });
     }
 
   navegarAEnvioDinero() {
@@ -23,5 +34,4 @@ export class WelcomeComponentComponent implements OnInit{
   navegarARegistro() {
     this.router.navigate(['/registro'])
   }
-
 }
